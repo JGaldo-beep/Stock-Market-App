93381ba75b4ba4212514c9f2568cd1b68c9dbc79
Juan Galdo <galdiros@gmail.com>
2024-05-26 22:09:13 -0500

    API & DB Setup

---
+++ app/src/main/java/com/example/stockmarket/data/local/CompanyListingEntity.kt
@@ -0,0 +1,14 @@
+package com.example.stockmarket.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class CompanyListingEntity (
+    @PrimaryKey
+    val id: Int? = null,
+
+    val symbol: String,
+    val name: String,
+    val exchange: String
+)
--- app/src/main/java/com/example/stockmarket/data/remote/StockApi.kt
+++ app/src/main/java/com/example/stockmarket/data/remote/StockApi.kt
@@ -1,4 +1,19 @@
 package com.example.stockmarket.data.remote
 
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
 interface StockApi {
+
+    @GET("query?function=LISTING_STATUS")
+    suspend fun getListings(
+        @Query("apikey") apiKey: String
+    ) : ResponseBody
+
+    companion object {
+        const val API_KEY = "AB1MIG3AGMKOWWTP"
+        const val BASE_URL = "https://alphavantage.co"
+    }
 }
